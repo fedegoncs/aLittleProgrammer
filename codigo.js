0db0f43caae73cb6187cf70122ac338d3ec45f5e
fedegoncs <79818380+fedegoncs@users.noreply.github.com>
2021-02-28 23:14:14 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+document.querySelector('.menu-btn').addEventListener('click', () => {
+	document.querySelector('.nav-menu').classList.toggle('show');
+});
+ScrollReveal().reveal('.showcase', {delay: 500});
+ScrollReveal().reveal('.news-cards', {delay: 750});
+ScrollReveal().reveal('.cards-banner-one', {delay: 1000});
+ScrollReveal().reveal('.cards-banner-two', {delay: 1000});
