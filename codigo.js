cb4108d6a52f1a548b5d595f5e73466a5fb30e27
fedegoncs <79818380+fedegoncs@users.noreply.github.com>
2021-03-04 03:06:03 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -6,3 +6,15 @@ ScrollReveal().reveal('.showcase', {delay: 500});
 ScrollReveal().reveal('.news-cards', {delay: 750});
 ScrollReveal().reveal('.cards-banner-one', {delay: 1000});
 ScrollReveal().reveal('.cards-banner-two', {delay: 1000});
+
+
+nombre = prompt("¿Cuál es tu nombre?");
+
+if (nombre == "magali" || nombre == "magui" || nombre == "Magali" || nombre ==  "Magui" || nombre == "maga" || nombre == "Maga" || nombre == "magi" || nombre == "Magi" || nombre =="tirri"){
+	alert("Te amo muchisimo " + (nombre));
+	alert("y sos lo más lindo de este mundo vos " + (nombre));
+}
+
+else if (nombre != "magali" || nombre != "magui" || nombre != "Magali" || nombre !=  "Magui" || nombre != "maga" || nombre != "Maga" || nombre != "magi" || nombre != "Magi")
+	alert("Hola " + (nombre) + ", esta pagina fue creada por el Momoide y Picaporte");
+
