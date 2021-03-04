674279d3f97db182857282e02e142dc3dfc82912
fedegoncs <79818380+fedegoncs@users.noreply.github.com>
2021-03-03 22:47:44 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,6 +1,6 @@
 console.log("hello world");
 document.querySelector('.menu-btn').addEventListener('click', () => {
-	document.querySelector('.nav-menu').classList.toggle('hide');
+	document.querySelector('.nav-menu').classList.toggle('show');
 });
 ScrollReveal().reveal('.showcase', {delay: 500});
 ScrollReveal().reveal('.news-cards', {delay: 750});
